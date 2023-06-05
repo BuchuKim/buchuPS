0e293477f39ddc8677996ae6117f4facd69f7a59
김부추 <qkqdlek32@gmail.com>
2023-06-05 11:18:16 +0900

    다단계 칫솔 판매

---
+++ Programmers/다단계 칫솔 판매.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def solution(enroll, referral, seller, amount):
+    children,money = {p:set() for p in enroll},{p:0 for p in enroll}
+    parent = {enroll[i]:referral[i] for i in range(len(enroll))}
+
+    for i in range(len(enroll)):
+        if referral[i]!="-":
+            children[referral[i]].add(enroll[i])
+
+    for i in range(len(seller)):
+        person, profit = seller[i], amount[i]*100
+        while (person!="-"):
+            money[person] += profit
+            up = int(profit * 0.1)
+            if (up==0):
+                break
+            money[person] -= up
+            profit = up
+            person = parent[person]
+
+    return [money[p] for p in enroll]
+        
