8fd432c84a232d4e6e59e7e0e58a9a0c3895c02d
김부추 <qkqdlek32@gmail.com>
2023-06-09 15:32:40 +0900

    20230609 등산 코스 정하기

--- Programmers/등산.py
+++ Programmers/등산.py
@@ -1,32 +1,30 @@
-from collections import defaultdict,deque
-import sys
+from collections import defaultdict
+from heapq import heappop,heappush
 def solution(n, paths, gates, summits):
-    # 출입구 -> 한 번 산봉우리 -> 출입구에서 intensity 최소
-    answer = [-1,sys.maxsize] # [산봉우리 번호, intensity] return
-    path,gates,summits = defaultdict(list),set(gates),set(summits)
+    answer = [50_000,10_000_000]
+    summits,gates,path = set(summits),set(gates),defaultdict(list)
     for i,j,w in paths:
-        path[i].append((j,w))
-        path[j].append((i,w))
-
-    Q = deque()
-    for gate in gates:
-        Q.append((gate,0,set([gate])))
-    
-    # BFS
-    while Q:
-        cur,intensity,visited = Q.popleft()
-        for node,dis in path[cur]:
-            if (node in visited) or (node in gates):
-                continue
-                
-            c_intensity = max(dis,intensity)
-            if node in summits:
-                if (c_intensity<answer[1]):
-                    answer = [node,c_intensity]
-                elif (c_intensity==answer[1]):
-                    answer[0] = min(node,answer[0])
-                continue
+        path[i].append((w,j)); path[j].append((w,i))
+        
+    intensities = [10_000_001 for _ in range(n+1)]
+    heap,visited = [(0,s) for s in gates],[False for _ in range(n+1)]
+    while heap:
+        dis,node = heappop(heap)
+        visited[node] = True
+        if (dis > answer[1]):
+            continue
             
-            n_visited = set(visited); n_visited.add(node)
-            Q.append((node,c_intensity,n_visited))
+        for n_dis,n_node in path[node]:
+            if visited[n_node] or n_node in gates:
+                continue
+            intensity = max(n_dis,dis)
+            if (intensities[n_node] > intensity):
+                intensities[n_node] = intensity
+                if (n_node in summits):
+                    if (intensity < answer[1]):
+                        answer = [n_node,intensity]
+                    elif (intensity==answer[1]):
+                        answer[0] = min(answer[0],n_node)
+                else:
+                    heappush(heap,(intensity,n_node))    
     return answer
